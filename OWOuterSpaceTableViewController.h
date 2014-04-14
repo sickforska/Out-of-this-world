013c9a8503b5a80c6263eb3133a6528619f816ce
Cooper Richardson <richardson.cooper@gmail.com>
2014-04-13 17:06:28 -0700

    First Commit

---
+++ OWOuterSpaceTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  OWOuterSpaceTableViewController.h
+//  Out of this world
+//
+//  Created by Cooper Richardson on 4/11/14.
+//  Copyright (c) 2014 Cooper RIchardson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWOuterSpaceTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray *planets;
+
+@end
