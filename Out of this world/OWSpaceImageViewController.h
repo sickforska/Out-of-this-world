b7e9f1b08ef2a153eb36deea17f1fc9d003cbff7
Cooper Richardson <richardson.cooper@gmail.com>
2014-04-13 19:31:12 -0700

    Added new ui scroll view

---
+++ Out of this world/OWSpaceImageViewController.h
@@ -0,0 +1,15 @@
+//
+//  OWSpaceImageViewController.h
+//  Out of this world
+//
+//  Created by Cooper Richardson on 4/13/14.
+//  Copyright (c) 2014 Cooper RIchardson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWSpaceImageViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@end
---
+++ Out of this world/OWSpaceImageViewController.m
@@ -0,0 +1,49 @@
+//
+//  OWSpaceImageViewController.m
+//  Out of this world
+//
+//  Created by Cooper Richardson on 4/13/14.
+//  Copyright (c) 2014 Cooper RIchardson. All rights reserved.
+//
+
+#import "OWSpaceImageViewController.h"
+
+@interface OWSpaceImageViewController ()
+
+@end
+
+@implementation OWSpaceImageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
